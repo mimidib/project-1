16d193d6ec96c1acf37bba7fe8346d36d9e19e05
Mireille D <mireille.d@fdmgroup.com>
2023-03-03 12:55:04 -0500

    added s3 bucket and outputs

--- variable.tf
+++ variable.tf
@@ -1,8 +1,11 @@
 variable "users" {
-  default = ["Gregory Daughtry", "Ryan deMos", "Jarret Nachtigal", "Adeyemi Oladosu"]
+  default = ["Gregory-Daughtry", "Ryan-deMos", "Jarret-Nachtigal", "Adeyemi-Oladosu"]
 }
 
 variable "DEV-22-bucket-names" {
   default = "my-DEV-22-bucket"
 }
 
+variable "number-of-DEV-22-buckets" {
+  default = 2
+}
